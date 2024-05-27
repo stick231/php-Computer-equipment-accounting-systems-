fe51050f54ebc1b0c68d0f5864a449f60cc00fe2
stick231 <veniamin7766z@gmail.com>
2024-05-27 16:00:57 +0300

    add create functional

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-//реализация read
-//create
-//delete
-//update
+document.getElementById("button-create").addEventListener("click", function(event) {
+    event.preventDefault();
+    const form = document.getElementById('deviceForm');
+    const formData = new FormData(form);
+
+    fetch('create.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        // Вставляем ответ сервера в DOM
+        const successMessage = document.createElement('div');
+        successMessage.innerHTML = data;
+        document.body.appendChild(successMessage);
+    })
+    .catch(error => {
+        alert('Произошла ошибка: ' + error.message);
+    });
+})
