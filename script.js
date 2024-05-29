162387dea69bcb383005c042981d803658f7aaf9
stick231 <veniamin7766z@gmail.com>
2024-05-29 17:37:00 +0300

    create update functional

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function fetchDevices() {
                     <td>${device.serial_number}</td>
                     <td>${device.purchase_date}</td>
                     <td>
-                        <button class="edit-btn">Редактировать</button>
+                        <button class="edit-btn" data-device-id="${device.id}">Редактировать</button>
                         <button class="delete-btn" data-device-id="${device.id}">Удалить</button>
                     </td>
                 `;
@@ -59,6 +59,9 @@ document.addEventListener('click', function(event) {
     if (event.target.classList.contains('delete-btn')) {
         const deviceId = event.target.dataset.deviceId;
         deleteDevice(deviceId);
+    } else if (event.target.classList.contains('edit-btn')) {
+        const deviceId = event.target.dataset.deviceId;
+        editDevice(deviceId);
     }
 });
 
@@ -75,6 +78,48 @@ function deleteDevice(deviceId) {
         if (data.success) {
             alert(data.message);
             fetchDevices(); // Обновляем таблицу после удаления
+        } else {
+            alert(data.message);
+        }
+    })
+    .catch(error => {
+        alert('Произошла ошибка: ' + error.message);
+    });
+}
+
+function editDevice(deviceId) {
+    document.getElementById('device_type').value = "";
+    document.getElementById('manufacturer').value = "";
+    document.getElementById('model').value = "";
+    document.getElementById('serial_number').value = "";
+    document.getElementById('purchase_date').value = "";
+
+    document.getElementById('button-create').textContent = 'Сохранить';
+
+    document.getElementById('button-create').addEventListener('click', function(event) {
+        event.preventDefault();
+        updateDevice(deviceId);
+})
+}
+
+
+
+function updateDevice(deviceId) {
+    const form = document.getElementById('deviceForm');
+    const formData = new FormData(form);
+    formData.append('id', deviceId);
+
+    fetch(`update.php`, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert(data.message);
+            form.reset();
+            document.getElementById('button-create').textContent = 'Добавить устройство';
+            fetchDevices();
         } else {
             alert(data.message);
         }
--- update.php
+++ update.php
@@ -1,4 +1,32 @@
 <?php
 require_once 'db.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $deviceType = $_POST['device_type'];
+    $manufacturer = $_POST['manufacturer'];
+    $model = $_POST['model'];
+    $serialNumber = $_POST['serial_number'];
+    $purchaseDate = $_POST['purchase_date'];
+    $id = $_POST['id'];
+
+    $query = "UPDATE devices SET device_type = ?, manufacturer = ?, model = ?, serial_number = ?, purchase_date = ? WHERE id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("sssssi", $deviceType, $manufacturer, $model, $serialNumber, $purchaseDate, $id);
+
+    if ($stmt->execute()) {
+        $response = array(
+            'success' => true,
+            'message' => 'Устройство успешно обновлено'
+        );
+        echo json_encode($response);
+    } else {
+        $response = array(
+            'success' => false,
+            'message' => 'Ошибка при обновлении устройства: ' . $stmt->error
+        );
+        echo json_encode($response);
+    }
+
+    $stmt->close();
+}
 ?>
