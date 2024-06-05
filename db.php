0269b17f47e56c0c9111482ac29f20ff348db88e
stick231 <veniamin7766z@gmail.com>
2024-06-05 18:11:58 +0300

    Fix

--- db.php
+++ db.php
@@ -1,13 +1,19 @@
 <?php
-$host = 'localhost';
-$db = 'inventory';
-$user = 'root';
-$pass = '';
+$user = "root";
+$pass = "";
+$db1 = "users";
+$db2 = "inventory";
+$host = "localhost";
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db1);
+$mysqli = new mysqli($host, $user, $pass, $db2);
 
-if ($mysqli->connect_error) {
-    die('Connection Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+if($conn->connect_error){
+    die('Ошибка подключения (' . $conn1->connect_errno . ') ' . $conn1->connect_error);
+}
+
+if($mysqli->connect_error){
+    die('Ошибка подключения (' . $conn2->connect_errno . ') ' . $conn2->connect_error);
 }
 ?>
       
