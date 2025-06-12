508015fffe44c58b1564684c2c0e9be9fd6a6691
stick231 <veniamin7766z@gmail.com>
2025-06-13 00:17:30 +0300

    added initial migration and deleted useless settings nginx

---
+++ migration.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Entities\Database;
+
+$database = (new Database())->getConnection();
+
+$query = "
+-- Adminer 5.2.1 MySQL 8.0.42 dump
+
+SET NAMES utf8;
+SET time_zone = '+03:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+SET foreign_key_checks = 0; 
+
+CREATE TABLE `devices` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `device_type` varchar(255) NOT NULL,
+  `manufacturer` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `serial_number` varchar(255) NOT NULL,
+  `purchase_date` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `serial_number` (`serial_number`),
+  KEY `idx_user_id` (`user_id`),
+  CONSTRAINT `fk_devices_user`
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SET foreign_key_checks = 1;
+
+";
+
+$stmt = $database->prepare($query);
+$stmt->execute();
